8a8804ec943067b04ff40c980d753d60c3582353
Khushboo Hussain <khushboohussain77@gmail.com>
2019-06-25 16:48:29 +0500

    job creation

--- src/app/employer/ads/create/step1/step1.page.ts
+++ src/app/employer/ads/create/step1/step1.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActionSheetController, NavController } from '@ionic/angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from 'src/app/services/api.service';
+import { HelperService } from 'src/app/services/helper.service';
 
 @Component({
   selector: 'app-step1',
@@ -11,22 +12,27 @@ import { ApiService } from 'src/app/services/api.service';
 export class Step1Page implements OnInit {
 
   form: FormGroup;
+  // data: any;
+  actionController: boolean;
 
-  constructor(public actionSheetController: ActionSheetController, private navController: NavController, private fb: FormBuilder, private api: ApiService) {}
 
-  
+  constructor(public actionSheetController: ActionSheetController, private navController: NavController, private fb: FormBuilder, private api: ApiService, private helper: HelperService) { }
+
+
 
   ngOnInit() {
     this.form = this.fb.group({
       jobTitle: ['', Validators.required],
       address: ['', Validators.required],
-      startDate: ['', Validators.required], 
+      startDate: ['', Validators.required],
       endDate: ['', Validators.required],
       continoueWork: ['', Validators.required],
       fastReply: ['', Validators.required]
     })
   }
-  submit(form) {
+
+
+  submit(form: any) {
     let data = {
       jobTitle: form.value.jobTitle,
       address: form.value.address,
@@ -35,8 +41,37 @@ export class Step1Page implements OnInit {
       continoueWork: form.value.continoueWork,
       fastReply: form.value.fastReply,
     }
-    console.log(data);
+    // Put the object into storage
+    localStorage.setItem('testObject', JSON.stringify(data));
+
+    // localStorage.setItem('AdsData', this.data);
+       // let id = localStorage.getItem('uid');
+    // console.log('Current User Id is' + localStorage.getItem('uid'));
+
     this.adOptions();
+ 
+
+    // this.api.createAds(localStorage.getItem('uid'), {
+    //   jobTitle: this.data.value.jobTitle,
+    //   address: this.data.address,
+    //   startDate: this.data.startDate,
+    //   endDate: this.data.value.endDate
+    // }).then(res => {
+    //   if (this.data.continoueWork == '' || this.data.continoueWork == false) {
+    //     this.continousCheck = false;
+    //   }
+    //   else {
+    //     this.continousCheck = true;
+    //   }
+    //   this.helper.presentToast("")
+    // }, err => {
+    //   this.helper.presentToast(err.message + 'Error!');
+    // });
+
+
+    // var test = JSON.parse(localStorage.getItem("continousCheck"));
+    // console.log(typeof test);
+    // console.log(test); 
   }
   async adOptions() {
     const actionSheet = await this.actionSheetController.create({
@@ -45,11 +80,16 @@ export class Step1Page implements OnInit {
         text: 'Alle Termine gleich',
         handler: () => {
           this.navController.navigateForward("/employer/ads/create/step2");
+
+          localStorage.setItem('actionController', JSON.stringify(this.actionController = true));
         }
       }, {
         text: 'Einzelne Termine bearbeiten',
         handler: () => {
           this.navController.navigateForward("/employer/ads/create/step2");
+          // this.actionController = false;
+          localStorage.setItem('actionController', JSON.stringify(this.actionController = false));
+
         }
       }, {
         text: 'Abbrechen',
--- src/app/employer/ads/create/step2/step2.page.ts
+++ src/app/employer/ads/create/step2/step2.page.ts
@@ -8,7 +8,47 @@ import { NavController } from '@ionic/angular';
 })
 export class Step2Page implements OnInit {
 
-  x = false;
+  data: any;
+  continousCheck: boolean;
+  option1: boolean;
+  option2: boolean;
+  actionController: boolean;
+
+
+  ngOnInit() {
+
+    // Retrieve the object from storage
+    this.data = JSON.parse(localStorage.getItem('testObject'));
+    // console.log('retrievedObject: \n', this.data);
+
+    this.actionController = JSON.parse(localStorage.getItem("actionController"));
+    // console.log(typeof this.actionController);
+    console.log("actionController check is " + this.actionController);
+    console.log("option1 is " + this.option1);
+
+    // checking conditions for showing proper template
+    if (this.actionController == false && (this.data.continoueWork == '' || this.data.continoueWork == false)) {
+      this.continousCheck = true;
+      this.option1 = false;
+      this.option2 = false;
+
+    }
+    if (this.actionController == true && this.data.continoueWork == true) {
+      this.option2 = true;
+      this.option1 = false;
+      this.continousCheck = false;
+      // localStorage.setItem('continousCheck', JSON.stringify(this.continousCheck));
+      // localStorage.setItem('option1', JSON.stringify(this.option1));
+      // localStorage.setItem('actionController', JSON.stringify(this.actionController));
+    }
+    if (this.actionController == true && (this.data.continoueWork == '' || this.data.continoueWork == false)) {
+      this.option1 = true;
+      this.option2 = false;
+      this.continousCheck = false;
+    }
+
+
+  }
 
   constructor(private navController: NavController) { }
 
@@ -16,7 +56,6 @@ export class Step2Page implements OnInit {
     this.navController.navigateRoot("/employer/ads/create/step3");
   }
 
-  ngOnInit() {
-  }
+
 
 }
